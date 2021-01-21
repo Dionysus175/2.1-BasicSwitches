705cab0526a201a37dce30c3b1ed740316ac1d5f
Dion <K00248714@student.lit.ie>
2021-01-21 10:57:50 +0000

    Coding Changes

---
+++ Assets/Scripts/BulbController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulbController : MonoBehaviour
+{
+    private Animator bulbAnimator;
+    public bool bulbOn;
+    // Start is called before the first frame update
+    void Awake ()
+    {
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void turnOn()
+    {
+
+    }
+
+    public void turnOff()
+    {
+
+    }
+}
